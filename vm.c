71eb51ac793095686517bc80e8174fbcd201af1a
NathanTcz <nathancazell@gmail.com>
2014-04-16 17:13:39 -0400

    initial commit

--- vm.c
+++ vm.c
@@ -336,39 +336,6 @@ bad:
   return 0;
 }
 
-/*
- * COPY ON WRITE IMPLEMENTATION
-*/
-/*pde_t*
-copyuvm_cow(pde_t *pgdir, uint sz)
-{
-  pde_t *d;
-  pte_t *pte;
-  uint pa, i, flags;
-  char *mem;
-
-  if((d = setupkvm()) == 0)
-    return 0;
-  for(i = 0; i < sz; i += PGSIZE){
-    if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
-      panic("copyuvm: pte should exist");
-    if(!(*pte & PTE_P))
-      panic("copyuvm: page not present");
-    pa = PTE_ADDR(*pte);
-    flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto bad;
-    memmove(mem, (char*)p2v(pa), PGSIZE);
-    if(mappages(d, (void*)i, PGSIZE, v2p(mem), flags) < 0)
-      goto bad;
-  }
-  return d;
-
-bad:
-  freevm(d);
-  return 0;
-}*/
-
 //PAGEBREAK!
 // Map user virtual address to kernel address.
 char*
